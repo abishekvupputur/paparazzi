f8b66f3b9071f161db7a1ac27131911a54692c7d
Abishek Narasimhan <abishekvupputur@gmail.com>
2019-03-08 11:34:32 +0100

    Add fix_path script for anaconda bug

---
+++ fix_path.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export PATH=/opt/ros/melodic/bin:/home/bigfoot/.local/bin:/home/bigfoot/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+
